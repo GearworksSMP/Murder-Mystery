6440b219aec9f1d3a287efc6bb288e8ac5f818b7
uberswe <m@rkus.io>
2024-10-26 15:57:16 +0200

    Update to 1.20.1

--- src/main/java/net/smelly/murdermystery/game/map/MMMapGenerator.java
+++ src/main/java/net/smelly/murdermystery/game/map/MMMapGenerator.java
@@ -8,8 +8,11 @@ import net.minecraft.world.biome.Biome;
 import xyz.nucleoid.map_templates.MapTemplate;
 import xyz.nucleoid.map_templates.MapTemplateSerializer;
 import xyz.nucleoid.plasmid.game.GameOpenException;
+import net.minecraft.world.biome.BiomeKeys;
 
 import java.io.IOException;
+
+import static net.smelly.murdermystery.MurderMystery.LOGGER;
 
 /**
  * @author SmellyModder (Luke Tonon)
@@ -25,7 +28,14 @@ public record MMMapGenerator(MMMapConfig config) {
 		}
 
 		MMMap map = new MMMap(template, this.config);
-		template.setBiome((RegistryKey<Biome>) RegistryKey.INSTANCES.get((RegistryKeys.BIOME.getValue() + ":" + this.config.biome()).intern()));
+		String biomeKey = RegistryKeys.BIOME.getValue() + ":" + this.config.biome();
+		String biomeObject = biomeKey.intern();
+        LOGGER.info("{} biome: {}", biomeKey, biomeObject);
+		if (biomeKey.equals("minecraft:worldgen/biome:minecraft:dark_forest")) {
+			template.setBiome(BiomeKeys.DARK_FOREST);
+		} else {
+			template.setBiome(BiomeKeys.MEADOW);
+		}
 		return map;
 	}
 }
